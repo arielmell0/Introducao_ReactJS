a97a8e6fbc160f54d5b1800bcf899f22c8798081
arielmell0 <arielmello@rede.ulbra.br>
2022-04-27 05:40:21 -0300

    feat: Manipulando Eventos
    como utilizar eventos no react

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,18 @@
 import React from "react";
 
-const listCustomer = [
-    {
-        id: 1,
-        name: 'Ariel Oliveira de Mello',
-        skills: ['React', 'Node', 'CSS', 'WebPack']
-    },
-    {
-        id: 2,
-        name: 'Adriana Pereira de Oliveira',
-        skills: ['Cobol', 'Golang', 'Insomnia', 'Python']
-    },
-    {
-        id: 3,
-        name: 'Silvio Alves de Mello',
-        skills: ['Angular', 'Electron', 'Elixir', 'Ruby']
-    },
-    {
-        id: 4,
-        name: 'Julia da Silva Schardosim',
-        skills: ['Express', 'MongoDB', 'MySQL', 'JWT']
-    },
-]
-
 const App = () => {
-    const renderCustomers = (customer, index) => {
-        return (
-            <div>
-                <li key={`customer-${customer.id}`}>{customer.name}</li>
-                {customer.skills.map(renderSkills)}
-            </div>      
-        )
+    const showEvent = (e) => {
+        console.log('Evento clickado')
+        console.log(e)
     }
 
-    const renderSkills = (skill, index) => {
-        return (
-            <div style={{ paddingLeft: '30px' }}  key={`skill-${index}`}>
-                <li>{skill}</li>
-            </div>
-        )
-    }
-
+    const Button = <button onClick={showEvent}>Mostrar evento</button>
+    
     return (
         <div>
             <p>Digital Innovation One</p>
             <p>Bem vindo a nossa aula =D</p>
-            <div>
-                <ul>{listCustomer.map(renderCustomers)}</ul>
-            </div>
+            {Button}
         </div>
     )
 }
