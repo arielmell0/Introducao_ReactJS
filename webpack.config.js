8732f47350e513bc3cbe6a18605eadc8595e5318
arielmell0 <arielmello@rede.ulbra.br>
2022-04-27 01:56:47 -0300

    feat: Instalação e configuração do ESLint
    instalação do eslint para correção dos erros via cli, caso queira em tempo real instalar plugin do vscode. Adição do sourcemap no webpack.config o que nos ajuda a rastrear erros no source do browser com muita facilidade

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HTMLWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: "source-map",
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
