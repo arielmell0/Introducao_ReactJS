768d98f1f79a3d7082ca98c6f8ebcd4dfd239a6c
arielmell0 <arielmello@rede.ulbra.br>
2022-04-27 01:16:15 -0300

    feat: Aprenda a configurar o webpack
    webpack e babel configurados. script para inicialização é o start:dev

--- src/index.js
+++ src/index.js
@@ -1,42 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDom from "react-dom";
-import "./style.css";
+import App from "./App.jsx";
 
-class App extends Component {
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            clock: 1000,
-            copo: 'água'
-        }
-    }
-
-    componentDidMount = () => {
-        window.setTimeout(() => {
-            this.setState({
-                copo: 'suco'
-            })
-        }, 3000)
-    }
-
-    alterarCopo = () => {
-        this.setState({
-            copo: 'refrigerante'
-        })
-    }
-
-    render() {
-        const { clock, copo } = this.state
-        return (
-            <div>
-                <h1>{clock}</h1>
-                <button onClick={() => this.alterarCopo()}><h1>{copo}</h1></button>
-            </div>
-        )
-    }
-}
-
-const rootElement = document.getElementById("root");
-ReactDom.render(<App />, rootElement)
+ReactDom.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path')
+const HTMLWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './teste.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundler.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
 }
