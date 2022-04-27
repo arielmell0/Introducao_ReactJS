1d6e8cba7caf998e8d6e8c072f56346466baa470
arielmell0 <arielmello@rede.ulbra.br>
2022-04-27 01:32:29 -0300

    feat: Aprenda a configurar o webpack
    adicionando mais um botão ao index.js e tenstando o hot-reload da nossa aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Button from './Button';
 
 const App = () => {
     return (
         <div>
             <p>Digital Innovation One</p>
             <p>Bem vindo a nossa aula =D</p>
+            <Button/>     
         </div>
     )
 }
---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Button = () => (
+    <button>Olá =D</button>
+)
+
+export default Button
